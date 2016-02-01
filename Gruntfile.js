4058b5a092bc496b79ea3e91acb4b4787c912b2f
ikosenn <i@kosenn.me>
2016-02-01 13:12:38 +0300

    Remove trailing space

--- Gruntfile.js
+++ Gruntfile.js
@@ -604,7 +604,7 @@ module.exports = function ( grunt ) {
      * The `build` task gets your app ready to run for development and testing.
      */
     grunt.registerTask( "build", [
-        "clean", "html2js", "jshint", "jscs", 
+        "clean", "html2js", "jshint", "jscs",
         "sass:build", "concat:build_css",
         "copy:build_app_assets", "copy:build_vendor_assets",
         "copy:build_app_settings", "copy:build_appjs", "copy:build_vendorjs",
